dcc1f87559128ef9b82e06f3cfd2326e4c27fb98
Cannon <cantheman2468@gmail.com>
2018-07-02 23:47:21 -0600

    forgot to save -- same as prev

--- js/script.js
+++ js/script.js
@@ -46,22 +46,6 @@ function logoHover() {
     "images/logo/backslash logo_BackSlash 3 White.png";
 }
 
-$(document).ready(function($) {
-  // Your code goes here
-  console.log("working");
-  document.getElementById("imageNormal").addEventListener("mouseover", () => {
-    console.log("event");
-    document.getElementById("imageNormal").src =
-      "images/logo/backslash logo_BackSlash 3 White.png";
-  });
-
-  document.getElementById("imageNormal").addEventListener("mouseout", () => {
-    console.log("eventoff");
-    document.getElementById("imageNormal").src =
-      "images/logo/backslash logo_BackSlash 3 Grey.png";
-  });
-});
-
 //ie detection
 var isIE =
   !!navigator.userAgent.match(/Trident/g) ||
